ec9aa106a7ac8e9ee49000f131a9766dda9d5cf3
Kamil Salawa <emir.salawa@gmail.com>
2020-10-11 20:35:27 +0100

    added ingredient lits

---
+++ calloriesCouter.js
@@ -0,0 +1,60 @@
+/*
+KSalawa
+App counting calories in a dish prepared, counting nutritio nand calories of individual ingredients, and a portion of a ready meal.
+11 october 2020, writen while learning to code on codecademy.com
+*/
+
+
+/*
+ingredients used:
+
+100ml olive
+kcal: 821
+fat: 91.2
+
+
+500g tomato passata
+per 100g:
+kcal       28
+fat        0.1g
+carb       5.1g
+protein    1.4g
+
+
+400g ikea meatballs
+per 100g:
+kcal       254
+fat        20g
+carb       4g
+protein    14g
+
+450g mushrom
+per 100g:
+kcal       108
+fat        11g
+carb       0.4g
+protein    1.4g
+
+900g sauerkraut
+per 100g:
+kcal       13
+fat        0.5g
+carb       1g
+protein    0.8g
+
+280g kielbasa zywiecka
+per 100g:
+kcal       216
+fat        14g
+carb       0.5g
+protein    22g
+
+350g kielbasa tounska
+per 100g:
+kcal       268
+fat        9.2g
+carb       2.3g
+protein    13g
+
+*/
+
